3239eb7c4be5935f1f6edbaaf4346c224ed0fec2
junfengqiao <jfqemma@gmail.com>
2024-04-14 16:37:30 -0400

    part1 done for pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,34 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+// define an empty array and subarrays 
+// define startRow, endRow, startColumn, endColumn
+// loop through each row and wrte #
+function pyramid(n) {
+    let result = [];
+    for (let i = 0; i < n; i++){
+        result.push([])
+    }
+  
+    let startRow = 0;
+    let endRow = n-1;
+    let startColumn = Math.floor((2*n-1)/2)
+    let endColumn = Math.floor((2*n-1)/2)
+
+    while(startRow <= endRow){
+       
+        for(let i =0; i< 2*n-1; i++){
+            if(i>=startColumn && i<=endColumn){
+                result[startRow][i]='#'
+            } else {
+                result[startRow][i]=' '
+            }
+        }
+        startRow++;
+        endColumn++;
+        startColumn--;
+    }
+   return result;
+}
 
 module.exports = pyramid;
