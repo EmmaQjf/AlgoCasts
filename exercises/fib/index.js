66c34192efe66d09793b000823ac837533a0a37c
junfengqiao <jfqemma@gmail.com>
2024-04-14 07:50:52 -0400

    completed fib

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,18 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+
+//edge case, define the first and second element in the array
+//loop through the n, and add the number into the array
+// return the number in the position of n
+function fib(n) {
+    let array = [0,1]
+   if (n === 0) return 0;
+   if (n === 1 ) return 1;
+   for (let i = 2; i <=n; i++){
+    array[i]=array[i-1]+array[i-2]
+   }
+   return array[n]
+}
 
 module.exports = fib;
