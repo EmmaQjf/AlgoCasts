e95fc3b6879149942e81d4a6bcfb9e10e4cac662
junfengqiao <jfqemma@gmail.com>
2024-04-14 11:35:46 -0400

    complete maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,20 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+//frequency counter
+//define a fc object, maxValue =-definite
+//loop through the string and refine the object
+//find the property with the largest value
+function maxChar(str) {
+    let fc = { }
+    let maxValue = -infinity
+
+    for (let i =0; i<str.length; i++){
+        fc[str[i]]? fc[str[i]]+=1: fc[str[i]]=1;
+        fc[str[i]]>maxValue? maxValue=fc[str[i]]:maxValue=maxValue;
+    }
+    return fc.find(item => fc[item]===maxValue)
+
+}
 
 module.exports = maxChar;
