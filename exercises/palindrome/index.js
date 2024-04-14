31b19f47f378b6e6dc0c134309f63c4dc990792e
junfengqiao <jfqemma@gmail.com>
2024-04-14 15:38:59 -0400

    completed palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,15 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+// find the middle index of the string 
+//loop through the string and compare the letter from the beginning and end
+
+function palindrome(str) {
+    const midIndex = Math.floor(str.length-1)/2
+    for (let i =0; i<=midIndex; i++){
+        if(str[i] !== str[str.length -i-1]){return false}
+    }
+    return true
+}
 
 module.exports = palindrome;
