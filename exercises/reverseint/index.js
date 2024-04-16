b1a76790cc93f5a12b0129a7e4e2a2ca0760ebb7
junfengqiao <jfqemma@gmail.com>
2024-04-16 06:30:35 -0400

    completed reverseint

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,15 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
-
+// find the absolute value with Math.abs()
+//reverse function to reverse the string 
+function reverseInt(n) {
+    let absoluteN = Math.abs(n)
+  let str = absoluteN.toString().split('').reverse().join('')
+  let number = parseInt(str)
+   if(n<0){
+    number = -1*number
+   }
+   return  number
+}
 module.exports = reverseInt;
